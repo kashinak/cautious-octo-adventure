d88b7c37f812e6d59a24c332e078e4b34f3390f9
kashinak <kashinakfx@gmail.com>
2021-07-15 18:39:29 +0000

    add onclick to html files to enable new userClick js function to push game button user clicks into userSequence array using new playerSequence()

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,38 @@
+
+
 //declare game variables//
-let round = 0;
+// let round = 0;
 let score = 0;
 let highScore = [];
 const startButton = document.getElementById("start");
+const topleft = document.getElementById("topleft");
 let num = 4;
+let userSequence = [];
+let computerSequence = [];
 
 //upon clicking startButton, game console clears and new game sequence is played//
 
 startButton.addEventListener("click", startGame);
+topleft.addEventListener("click", userClick);
+topright.addEventListener("click", userClick);
+bottomright.addEventListener("click", userClick);
+bottomleft.addEventListener("click", userClick);
 
 function startGame() {
-    document.getElementById("start").innerHTML = "Start Game!";
-    document.getElementById("start").innerHTML = "";
+    score=0; /* any other variable initilized here*/
+    startButton.innerHTML = "Start Game!";
+    startButton.innerHTML = "";
     gameSequence();
+    playerSequence();
 };
 
 
 //game generates a sequence with a random tile click//
 function gameSequence() {
     init();
-    let n = Math.floor(Math.random() * num +1);
+    let n = Math.floor(Math.random() * num +1); //n is number of tiles to flash//
+    computerSequence.push(n);
+    console.log(computerSequence);
     setTimeout(() => {
         switch(n){
             case 1:
@@ -37,13 +50,14 @@ function gameSequence() {
             default:         
         }
     }, 1000);
-    end();
+    console.log(userSequence);
+    // end();
 };
 
 //end function 
-function end() {
-    startButton.style.display = "";
-};
+// function end() {
+//     startButton.style.display = ""; //remove entire outline of button//
+// };
 
 //link game photo tiles with corresponding game sounds and blink capability//
 function playSound1() {
@@ -51,26 +65,48 @@ function playSound1() {
     document.getElementById("game-photo-1").style.outline = '#ffff00 solid 10px';
     document.getElementById('beluga-sound').play();
     blink("game-photo-1");
+    // userSequence.push(0);
+    // compareSequences(computerSequence, userSequence);
 };
 
 function playSound2() { 
     document.getElementById("game-photo-2").style.outline = '#ffff00 solid 10px';
     document.getElementById('orca-sound').play();
     blink("game-photo-2");
+    // userSequence.push(1);
+    // compareSequences(computerSequence, userSequence);
 };
 
 function playSound3() {
     document.getElementById("game-photo-3").style.outline = '#ffff00 solid 10px';
     document.getElementById('rightwhale-sound').play();
     blink("game-photo-3");
+    // userSequence.push(2);
+    // compareSequences(computerSequence, userSequence);
 };
 
 function playSound4() {
     document.getElementById("game-photo-4").style.outline = '#ffff00 solid 10px';
     document.getElementById('spermwhale-sound').play();
     blink("game-photo-4");
+    // userSequence.push(3);
+    // compareSequences(computerSequence, userSequence);
 };
 
+function compareSequences(a,b) {
+    let sequenceLength === computerSequence.length;
+    for(let i=0; i < sequenceLength; i++) {
+        if (computerSequence[i] !== userSequence[i]){
+            startGame();
+            console.log("no match");
+        } else {
+            console.log("match");
+        }
+
+    }
+   
+ 
+}
 /*function init() to provide event listeners to make game buttons clickable and to playsound 
 and blink when player presses game button*/
 function init() {
@@ -78,7 +114,9 @@ function init() {
     document.getElementById("game-photo-2").addEventListener("click", playSound2);
     document.getElementById("game-photo-3").addEventListener("click", playSound3);
     document.getElementById("game-photo-4").addEventListener("click", playSound4);
-}
+    userSequence = [];
+    computerSequence = [];
+};
 
 /*blink function creates yellow outline border appear when player clicks a gameboard button and 
 then the border disappears after 1 second*/
@@ -98,27 +136,57 @@ function blink(id) {
             });
  };
 
+
  //add playerSequence to convert player game clicks to numbers in sequence array;
 function playerSequence() {
-    let playerClick = $(this).attr("id");
+    console.log(userSequence);
+    let playerClick = document.getElementById('id');
+    console.log(playerClick);
     switch(playerClick){
         case "topleft":
-            playerSequence.push(0);
-            startGame(0);
+            userSequence.push(0);
+            // startGame(0);
             break;
         case "topright":
-            playerSequence.push(1);
-            startGame(1);
+            userSequence.push(1);
+            // startGame(1);
             break;
 
         case "bottomleft":
-            playerSequence.push(2);
-            startGame(2);
+            userSequence.push(2);
+            // startGame(2);
             break;
 
         case "bottomright":
-            playerSequence.push(3);
-            startGame(3);
+            userSequence.push(3);
+            // startGame(3);
             break;
+            default:   
     }
 };
+
+function userClick(event) {
+    console.log(event.target.id);
+    let playerClick = document.getElementById(event.target.id);
+    console.log(playerClick);
+    switch(playerClick){
+        case "topleft":
+            userSequence.push(0);
+            // startGame(0);
+            break;
+        case "topright":
+            userSequence.push(1);
+            // startGame(1);
+            break;
+        case "bottomleft":
+            userSequence.push(2);
+            // startGame(2);
+            break;
+        case "bottomright":
+            userSequence.push(3);
+            // startGame(3);
+            break;
+            default:   
+    }
+
+}
